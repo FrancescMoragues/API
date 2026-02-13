b0b384f17f0622b43476162f7870bc17537ee9dc
FrancescMoragues <francescmoragues@paucasesnovescifp.cat>
2026-02-13 18:11:54 +0100

    Add alumne-controller to list enrolled students

---
+++ API/controllers/alumne-controller.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const Alumne = require('../models/alumne');
+
+exports.llistaAlumnesMatriculats = async (req, res) => {
+  // Llegeix el fitxer JSON
+  const alumnesJson = JSON.parse(fs.readFileSync('./src/data/alumnes.json', 'utf-8'));
+  
+  // Transforma a objectes Alumne
+  const alumnes = alumnesJson.map(alumne => 
+    new Alumne(alumne.nom, alumne.cognom, alumne.anys, alumne.cicle, alumne.moduls)
+  );
+
+  let alumnesFiltrats = [];
+
+  // Filtra per cicle IFC31-C i mòdul IAW
+  for (let i = 0; i < alumnes.length; i++) {
+    const alumne = alumnes[i];
+    if (alumne.cicle === "IFC31-C" && alumne.moduls.indexOf("IAW") > -1) {
+      alumnesFiltrats.push(alumne);
+    }
+  }
+
+  // Retorna el resultat
+  return res.json(alumnesFiltrats);
+};
