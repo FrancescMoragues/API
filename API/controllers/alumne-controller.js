223f2e497fad10c6d4a9222ede30ebdaff921d8b
FrancescMoragues <francescmoragues@paucasesnovescifp.cat>
2026-02-13 18:20:35 +0100

    Update alumne-controller.js

--- API/controllers/alumne-controller.js
+++ API/controllers/alumne-controller.js
@@ -2,25 +2,22 @@ const fs = require('fs');
 const Alumne = require('../models/alumne');
 
 exports.llistaAlumnesMatriculats = async (req, res) => {
-  // Llegeix el fitxer JSON
+  // Llegeix el fitxer JSON des de la teva estructura actual 
   const alumnesJson = JSON.parse(fs.readFileSync('./data/alumnes.json', 'utf-8'));
   
-  // Transforma a objectes Alumne
   const alumnes = alumnesJson.map(alumne => 
     new Alumne(alumne.nom, alumne.cognom, alumne.anys, alumne.cicle, alumne.moduls)
   );
 
   let alumnesFiltrats = [];
 
-  // Filtra per cicle IFC31-C i mòdul IAW
   for (let i = 0; i < alumnes.length; i++) {
     const alumne = alumnes[i];
+    // Filtre segons l'enunciat: IFC31-C i mòdul IAW [cite: 205, 305]
     if (alumne.cicle === "IFC31-C" && alumne.moduls.indexOf("IAW") > -1) {
       alumnesFiltrats.push(alumne);
     }
   }
 
-  // Retorna el resultat
-  return res.json(alumnesFiltrats);
+  return res.json(alumnesFiltrats); // Retorna el resultat [cite: 318]
 };
-
