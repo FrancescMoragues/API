de94e0e06b91b3d08260d4184d890244425d70a6
FrancescMoragues <francescmoragues@paucasesnovescifp.cat>
2026-02-13 18:19:34 +0100

    Update app.js

--- API/app.js
+++ API/app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
+// Importamos las rutas corregidas
 const alumneRoutes = require('./routes/alumne-routes');
 
 app.use(express.json());
 
-// Vincula les rutes dels alumnes
+// Vinculamos las rutas
 app.use('/api/alumnes', alumneRoutes);
-
 
 app.listen(3000, () => console.log('Servidor en http://localhost:3000'));
