3ec5f37555c82061b2a5276e4ab655afdb0f2bf2
FrancescMoragues <francescmoragues@paucasesnovescifp.cat>
2026-02-13 18:12:45 +0100

    Update alumne.js

---
+++ API/models/alumne.js
@@ -0,0 +1,11 @@
+class Alumne {
+  constructor (nom, cognom, anys, cicle, moduls) {
+    this.nom = nom;
+    this.cognom = cognom;
+    this.anys = anys;
+    this.cicle = cicle;
+    this.moduls = moduls;
+  }
+}
+
+module.exports = Alumne;
